0525c8ae6a5931596b3e41274cb57adf18568e8d
Phoenix Bishea <phoenixb@utexas.edu>
2016-03-11 03:50:17 -0600

    Add barrier script/object. Extend this script to force state in certain situations (force player to make a call).

---
+++ Assets/Scripts/Barrier.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Barrier : MonoBehaviour {
+
+	[SerializeField] private float audioCoolDown = 5.0f;
+	[SerializeField] private AudioClip clip;
+	private float timeSinceAudioLastPlayed;
+
+	void Start()
+	{
+		timeSinceAudioLastPlayed = clip.length + audioCoolDown;
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		timeSinceAudioLastPlayed += Time.deltaTime;
+//		print(timeSinceAudioLastPlayed);
+	}
+
+	void OnTriggerStay(Collider collision)
+	{
+		if (timeSinceAudioLastPlayed > (clip.length + audioCoolDown))
+		{
+			print("Playing sound");
+			AudioSource.PlayClipAtPoint(clip, transform.position);
+			timeSinceAudioLastPlayed = 0.0f;
+		}
+	}
+}
