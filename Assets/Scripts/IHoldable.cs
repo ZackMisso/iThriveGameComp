f49d867c49e9f481aeea84c2f2d937a604e2ae97
ZackMisso <zack441@mac.com>
2016-03-10 18:14:28 -0600

    Deleted Everything

--- Assets/Scripts/IHoldable.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-public interface IHoldable : IInteractable {
-	void OnExamine();
-	void OnUnExamine();
-	void OnThrow();
-	void OnExamineRotate();
-}
--- Assets/Scripts/IInteractable.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-public interface IInteractable {
-	void Highlighted(Material highlight);
-	void OnInteract();
-}
